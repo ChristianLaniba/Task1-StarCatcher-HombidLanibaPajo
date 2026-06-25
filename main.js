672b88375f1ab0376477bd11a30f0fe61276557b
ChristianLaniba <cLaniba@mcm.edu.ph>
2026-06-25 20:20:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -172,6 +172,8 @@ function create() {
             color: "#7f0000",
             fontStyle: "bold"
         });
+        
+        console.log("Game Over! Final score: " + this.score);
     };
 
     //spawn initial stars
